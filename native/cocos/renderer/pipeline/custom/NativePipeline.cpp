acaf202a6ca9921736d68cfba58c7887c760d351
MagicXHouse <2858000522@qq.com>
2026-09-08 11:41:20 +0800

    Changed the GeometryRenderer and DebugRenderer from using intrusive calls to registering callbacks (custom pipeline).

--- native/cocos/renderer/pipeline/custom/NativePipeline.cpp
+++ native/cocos/renderer/pipeline/custom/NativePipeline.cpp
@@ -48,6 +48,10 @@
     #include "profiler/DebugRenderer.h"
 #endif
 
+#if CC_USE_GEOMETRY_RENDERER
+    #include "cocos/renderer/pipeline/GeometryRenderer.h"
+#endif
+
 namespace cc {
 
 namespace render {
@@ -105,6 +109,47 @@ void addSubresourceNode<gfx::Format::DEPTH_STENCIL>(ResourceGraph::vertex_descri
 }
 
 } // namespace
+
+#if CC_USE_DEBUG_RENDERER
+namespace {
+class DebugRendererRenderCommand final : public CustomRenderCommand {
+public:
+    explicit DebugRendererRenderCommand(pipeline::PipelineSceneData *sceneData)
+    : _sceneData(sceneData) {}
+
+    void beginRenderCommand(const CustomRenderGraphContext &rg, RenderGraph::vertex_descriptor) override {
+        if (rg.currentRenderPass) {
+            DebugRenderer::getInstance()->render(rg.currentRenderPass, rg.primaryCommandBuffer, _sceneData);
+        }
+    }
+
+    void endRenderCommand(const CustomRenderGraphContext &, RenderGraph::vertex_descriptor) override {}
+
+private:
+    pipeline::PipelineSceneData *_sceneData{nullptr};
+};
+} // namespace
+#endif
+
+#if CC_USE_GEOMETRY_RENDERER
+class GeometryRendererRenderCommand final : public CustomRenderCommand {
+public:
+    explicit GeometryRendererRenderCommand(pipeline::PipelineSceneData *sceneData)
+    : _sceneData(sceneData) {}
+
+    void beginRenderCommand(const CustomRenderGraphContext &rg, RenderGraph::vertex_descriptor vertID) override {
+        const auto &sceneData = get(SceneTag{}, vertID, *rg.renderGraph);
+        if (sceneData.camera && sceneData.camera->getGeometryRenderer()) {
+            sceneData.camera->getGeometryRenderer()->render(rg.currentRenderPass, rg.primaryCommandBuffer, _sceneData);
+        }
+    }
+
+    void endRenderCommand(const CustomRenderGraphContext &, RenderGraph::vertex_descriptor) override {}
+
+private:
+    pipeline::PipelineSceneData *_sceneData{nullptr};
+};
+#endif
 
 NativePipeline::NativePipeline(const allocator_type &alloc) noexcept
 : device(gfx::Device::getInstance()),
@@ -1385,6 +1430,10 @@ bool NativePipeline::activate(gfx::Swapchain *swapchainIn) {
     pipelineSceneData->activate(device);
 #if CC_USE_DEBUG_RENDERER
     DebugRenderer::getInstance()->activate(device);
+    addCustomRenderCommand("debug-renderer", std::make_shared<DebugRendererRenderCommand>(getPipelineSceneData()));
+#endif
+#if CC_USE_GEOMETRY_RENDERER
+    addCustomRenderCommand("geometry-renderer", std::make_shared<GeometryRendererRenderCommand>(getPipelineSceneData()));
 #endif
     // generate macros here rather than construct func because _clusterEnabled
     // switch may be changed in root.ts setRenderPipeline() function which is after
